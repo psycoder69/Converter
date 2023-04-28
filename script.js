c657281b0e9278ea94099bbbc18ba58cef24f1e6
psycoder69 <zycus69@gmail.com>
2023-04-28 15:17:19 +0530

    added more units

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ let converter = document.getElementById('converter');
 
 function calculateConvertedValue(num, converter, reverse) {
     switch(converter) {
+        case "cm-to-foot":
+            return reverse == true
+            ? (num * 30.48).toFixed(2)
+            : (num / 30.48).toFixed(1);
+
         case "cm-to-inch":
-            return reverse == false
-            ? (num / 2.54).toFixed(2)
-            : (num * 2.54).toFixed(2);
+            return reverse == true
+            ? (num * 2.54).toFixed(2)
+            : (num / 2.54).toFixed(2);
 
         case "celsius-to-fahrenheit":
-            return reverse == false
-            ? ((9/5) * num + 32).toFixed(1)
-            : ((num - 32) * (5/9)).toFixed(1);
+            return reverse == true
+            ? ((num - 32) * (5/9)).toFixed(1)
+            : ((9/5) * num + 32).toFixed(1);
+
+        case "km-to-mile":
+            return reverse == true
+            ? (num * 1.609).toFixed(2)
+            : (num / 1.609).toFixed(2);
+
+        case "kg-to-pound":
+            return reverse == true
+            ? (num / 2.205).toFixed(2)
+            : (num * 2.205).toFixed(2);
     }
 }
 
@@ -40,6 +55,11 @@ converter.addEventListener("change", () => {
     let label2 = document.getElementById('label2');
 
     switch(converter.value) {
+        case "cm-to-foot":
+            label1.innerHTML = "Centimeter";
+            label2.innerHTML = "Foot";
+            break;
+
         case "cm-to-inch":
             label1.innerHTML = "Centimeter";
             label2.innerHTML = "Inch";
@@ -48,6 +68,16 @@ converter.addEventListener("change", () => {
         case "celsius-to-fahrenheit":
             label1.innerHTML = "&#176;Celsius";
             label2.innerHTML = "&#176;Fahrenheit";
+            break;
+
+        case "km-to-mile":
+            label1.innerHTML = "Kilometer";
+            label2.innerHTML = "Mile";
+            break;
+
+        case "kg-to-pound":
+            label1.innerHTML = "Kilogram";
+            label2.innerHTML = "Pound";
             break;
     }
 
